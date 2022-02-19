39b987a9de818c761c7739449e312146a216e0e4
JPedroCh <joaopedroaschaves@gmail.com>
2022-02-19 18:37:32 -0300

    Correçao da localizaçao das imagens e imports.
    Signed-off-by: JPedroCh <joaopedroaschaves@gmail.com>

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,6 +3,7 @@ import './App.css';
 import PlayButton from './Components/PlayButton';
 import ActualLocation from './Components/ActualLocation';
 import { BackgroundContainer } from './Style';
+import Mapa from './Images/Mapa/mapa.png';
 
 function HomePage () {
   var [homePage, setHomePage] = useState(true);
@@ -16,9 +17,16 @@ function HomePage () {
       <BackgroundContainer>
       <h1>Seja bem vindo ao Random Game!</h1>
       <h3>Encontre o menor caminho para chegar no destino final.</h3>
+        <img
+          src={Mapa}
+          alt="Mapa do jogo"
+          style={{
+            width: '40%',
+          }}
+        />
         <PlayButton onClick={() => { setHomePage(false) }}></PlayButton>
       </BackgroundContainer> :
-      <div><ActualLocation></ActualLocation></div>
+      <div><ActualLocation location="Cathedral"></ActualLocation></div>
       }
     </div>
   );
